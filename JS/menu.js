3720158270cf93c672bd82122a371cf7d28467ed
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-09 08:05:45 +0200

    Create a menu js file and assign the value to each variable

---
+++ JS/menu.js
@@ -0,0 +1,6 @@
+const html = document.getElementsByTagName('html')[0];
+const showBtn = document.querySelector('nav .show-menu-btn');
+const closedBtn = document.querySelector('nav .closed-menu-btn');
+const navMenu = document.querySelector('nav .nav_menu');
+const navLinks = document.querySelectorAll('nav .nav_menu li');
+
