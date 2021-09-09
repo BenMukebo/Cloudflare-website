67b4b2f1e2aed85795cbba219adbcdfd8e90e05e
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-09 12:41:11 +0200

    Creat data object and include all team data information

---
+++ JS/teamData.js
@@ -0,0 +1,62 @@
+const data = [
+  {
+    id: 1,
+    name: 'Albert Noah',
+    images: {
+      img: '../images/member1.png',
+      alt: 'team_1',
+    },
+    about: 'President Gen. of Cloudflare Company.',
+    work: 'Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.',
+  },
+  {
+    id: 2,
+    name: 'Albert Noah',
+    images: {
+      img: '../images/member2.png',
+      alt: 'team_2',
+    },
+    about: 'President Gen. of Cloudflare Company.',
+    work: 'Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.',
+  },
+  {
+    id: 3,
+    name: 'Albert Noah',
+    images: {
+      img: '../images/member3.png',
+      alt: 'team_3',
+      },
+    about: 'President Gen. of Cloudflare Company.',
+    work: 'Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.',
+  },
+  {
+    id: 4,
+    name: 'Albert Noah',
+    images: {
+        img: '../images/member4.png',
+        alt: 'team_4',
+      },
+    about: 'President Gen. of Cloudflare Company.',
+    work: 'Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.',
+  },
+  {
+    id: 5,
+    name: 'Albert Noah',
+    images: {
+      img: '../images/member3.png',
+      alt: 'team_3',
+    },
+    about: 'President Gen. of Cloudflare Company.',
+    work: 'Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.',
+  },
+  {
+    id: 6,
+    name: 'SohYeong Noh',
+    images: {
+      img: '../images/member6.png',
+      alt: 'team_6',
+    },
+    about: 'Head Manager of designers Company.',
+    work: 'Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.',
+  }, 
+];
